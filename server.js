198a183e4e339dd28bbf30f68a9df67e4295afc2
AniruddhPersi <135312191+AniruddhPersi@users.noreply.github.com>
2023-06-09 16:34:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 require("./routes/taskListRoute")(app)
+require("./routes/userRoute")(app)
 // app.get("/",(req,res)=>{
 //     res.send("welcome to modo")
 // });
